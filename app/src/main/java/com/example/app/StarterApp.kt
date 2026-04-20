02cc54753b2b1668b5569ed6cdb19964f46ceec4
ashishranjan27 <35250893+ashishranjan27@users.noreply.github.com>
2026-04-20 22:45:25 +0530

    Add comment about code push delay
    Added a comment explaining the delay in code push due to SSH key creation and company restrictions.

--- app/src/main/java/com/example/app/StarterApp.kt
+++ app/src/main/java/com/example/app/StarterApp.kt
@@ -3,5 +3,9 @@ package com.example.app
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+/**
+There was a delay in pushing the code because I had to create a new SSH key on my company laptop, 
+and there were several issues mainly due to restrictions from my current company. That’s why it took time to push the code.
+*/
 @HiltAndroidApp
 class StarterApp : Application()
